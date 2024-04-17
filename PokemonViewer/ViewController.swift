cdec8209411fead769a5e99c7947795110a84f0b
fuji37450 <fujii37450@gmail.com>
2024-04-17 13:10:14 +0800

    Initial Commit

---
+++ PokemonViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonViewer
+//
+//  Created by Claire Chung on 2024/4/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
